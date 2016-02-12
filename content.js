0d20da02556096373cabcfb8cb3273a4e76990f0
mk <mihasiksli@yandex.ru>
2016-02-12 16:02:48 +0300

    selector

--- content.js
+++ content.js
@@ -1,3 +1,60 @@
 /**
  * Created by mk on 12.02.16.
  */
+
+$(document).ready(function() {
+    $("body").append('<div id="selector-top">' +
+        '</div><div id="selector-bottom"></div>' +
+        '<div id="selector-left"></div>' +
+        '<div id="selector-right"></div>');
+
+
+    var selectors = {
+        top: $('#selector-top'),
+        left: $('#selector-left'),
+        right: $('#selector-right'),
+        bottom: $('#selector-bottom')
+    };
+
+    var offset = 4;
+
+    $(document).mousemove(function(event) {
+        if(event.target.id.indexOf('selector') !== -1 || event.target.tagName === 'BODY' || event.target.tagName === 'HTML') return;
+        var $target = event.target;
+        var targetOffset = $target.getBoundingClientRect();
+
+        selectors.top.css({
+           "top": targetOffset.top -1,
+            "left": targetOffset.left -1,
+            "width": targetOffset.width + 3
+        });
+
+        selectors.bottom.css({
+            "top": targetOffset.top + targetOffset.height + 1,
+            "left": targetOffset.left -1,
+            "width": targetOffset.width + 3
+        });
+
+        selectors.left.css({
+            "top": targetOffset.top -1,
+            "left": targetOffset.left -1,
+            "height": targetOffset.height + 3
+        });
+
+        selectors.right.css({
+            "top": targetOffset.top -1 ,
+            "left": targetOffset.left + targetOffset.width + 1,
+            "height": targetOffset.height +3
+        });
+        //console.log(targetOffset.top + " " + targetOffset.left);
+    });
+
+    $(document).click(function(event) {
+        console.log(event.target);
+        (event.target).style.pointerEvents = 'none';
+        return false;
+    });
+
+
+});
+
